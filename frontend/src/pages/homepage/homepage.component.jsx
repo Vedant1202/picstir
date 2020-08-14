aa5332737209bd0af439944a433b43fa4406ce8a
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-08-15 03:33:55 +0530

    Add homepage

---
+++ frontend/src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,8 @@
+/** @format */
+
+import React from 'react';
+import HomeFeed from '../../components/home-feed/home-feed.component';
+
+const Homepage = () => <HomeFeed></HomeFeed>;
+
+export default Homepage;
